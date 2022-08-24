303c7929ca7c75fe53ce90e27fb1cedd7ebc786c
Konstantin <97698560+Yurov123@users.noreply.github.com>
2022-08-24 20:50:01 +0300

    Add files via upload
    add info project

---
+++ build/precache-manifest.9d58690730ae69fe411672c22b3df1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a85bfa17a62361389d0",
+    "url": "/static/css/main.ed077c0a.chunk.css"
+  },
+  {
+    "revision": "2a85bfa17a62361389d0",
+    "url": "/static/js/main.2a85bfa1.chunk.js"
+  },
+  {
+    "revision": "d7d1df5415d14b8f7689",
+    "url": "/static/js/1.d7d1df54.chunk.js"
+  },
+  {
+    "revision": "e5e6b531a333e3c3049a",
+    "url": "/static/js/2.e5e6b531.chunk.js"
+  },
+  {
+    "revision": "5e7ffd2b0b64021c8666",
+    "url": "/static/js/3.5e7ffd2b.chunk.js"
+  },
+  {
+    "revision": "49383cc176920f0d2900",
+    "url": "/static/js/runtime~main.49383cc1.js"
+  },
+  {
+    "revision": "da0bd26e035b4b6b265215ad9729dc09",
+    "url": "/index.html"
+  }
+];
